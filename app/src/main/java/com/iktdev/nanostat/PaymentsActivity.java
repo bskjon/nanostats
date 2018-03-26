e3e5850810d87f31d9e5e9b0bc563e38c7f3dd08
Brage (Phoenix) <brage.skjonborg@hotmail.no>
2018-03-26 20:23:44 +0200

    Changed payments activity

--- app/src/main/java/com/iktdev/nanostat/PaymentsActivity.java
+++ app/src/main/java/com/iktdev/nanostat/PaymentsActivity.java
@@ -1,10 +1,16 @@
 package com.iktdev.nanostat;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ListView;
 
+import com.iktdev.nanostat.R;
 import com.iktdev.nanostat.core.nanopoolHandler;
 
 public class PaymentsActivity extends AppCompatActivity {
@@ -18,6 +24,17 @@ public class PaymentsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payments);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
 
         Intent intent = getIntent();
         Address = intent.getExtras().getString("address");
@@ -30,6 +47,13 @@ public class PaymentsActivity extends AppCompatActivity {
 
         nanopoolHandler nh = new nanopoolHandler();
         nh.applyPayments(this, nh.payments(Prefix, Address), (ListView) findViewById(R.id.paymentsListView), CryptoShort);
+
+        /* rv = (RecyclerView)getView().findViewById(R.id.fragment_overview_recyclerview);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        rv.setLayoutManager(linearLayoutManager);
+
+        rv.setHasFixedSize(false);*/
 
     }
 }
